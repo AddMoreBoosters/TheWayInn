56b76822885fa26fd4bb2597e60fc9f68a8620fc
Anna Ervik Hofseth <anna.e.hofseth@gmail.com>
2019-01-26 17:40:17 +0100

    Imported assets from previous version

---
+++ Assets/_Core/Scripts/DialogueManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets._Core.Scripts
+{
+	public class DialogueManager : MonoBehaviour
+	{
+		// Name of current speaker
+		// Slideshow-system for next text-slide
+		// Visual feedback for current speaker
+	}
+}
