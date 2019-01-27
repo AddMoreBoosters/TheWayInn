3e81b18b6c12aed0abc839ccb037f2133052e46b
Anna Ervik Hofseth <anna.e.hofseth@gmail.com>
2019-01-27 12:20:33 +0100

    Updated some prefabs, added audio and textures

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Assets
+{
+	public class AudioManager : MonoBehaviour
+	{
+		public AudioSource Background;
+		public AudioSource Effects;
+
+		public void PlayAudio(AudioClip clip)
+		{
+			Effects.PlayOneShot(clip);
+		}
+
+		public void SetLoopingEffect(AudioClip clip)
+		{
+			Effects.clip = clip;
+			Effects.loop = true;
+		}
+
+		public void StopLoopingEffect()
+		{
+			Effects.clip = null;
+		}
+
+		public void SetBackground(AudioClip clip)
+		{
+			Tweener bgFader = Background.DOFade(0, .2f);
+			bgFader.onComplete += delegate
+			{
+				Background.clip = clip;
+				bgFader.onComplete = null;
+				Background.DOFade(1, .2f);
+			};
+		}
+	}
+}
